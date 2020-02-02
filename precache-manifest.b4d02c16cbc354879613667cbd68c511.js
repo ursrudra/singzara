59c3d91dcee3afb0a85d048050feee711ca2cb60
Rudra Kishore <giduturirudrakishore@gmail.com>
2020-02-02 22:38:50 +0530

    Updates

---
+++ precache-manifest.b4d02c16cbc354879613667cbd68c511.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73e3c3eec72cf7fe30fddd836c3ff09e",
+    "url": "/singzara/index.html"
+  },
+  {
+    "revision": "8f6f52c32db71282aa5d",
+    "url": "/singzara/static/css/main.812ae51a.chunk.css"
+  },
+  {
+    "revision": "8b550cda57cd88a0d552",
+    "url": "/singzara/static/js/2.b5416d25.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/singzara/static/js/2.b5416d25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f6f52c32db71282aa5d",
+    "url": "/singzara/static/js/main.95ff51fa.chunk.js"
+  },
+  {
+    "revision": "0f136ae7bc0111378f4c",
+    "url": "/singzara/static/js/runtime-main.f7a68bbd.js"
+  }
+]);
